082a99164522a6e2fdfa7ea94a1ec5917c7ec6d6
rafinhaa <rafinhaa@me.com>
2022-08-15 13:46:35 -0300

    feat: Cria DefaultLayout

---
+++ src/Layout/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 90rem;
+  display: flex;
+  flex-direction: column;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 0 10rem;
+`;
