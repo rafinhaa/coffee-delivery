131be65a57510960e53c2f2fa66d368b15654187
rafinhaa <rafinhaa@me.com>
2022-08-15 13:46:54 -0300

    feat: Implementa LayoutDefault

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
+import { DefaultLayout } from "../Layout/DefaultLayout";
 import { Home } from "../pages/Home";
 import { Checkout } from "../pages/Checkout";
 import { Finished } from "../pages/Finished";
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/checkout" element={<Checkout />} />
-    <Route path="/finished" element={<Finished />} />
+    <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/finished" element={<Finished />} />
+    </Route>
   </Routes>
 );
