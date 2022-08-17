8f9ca1de3dca5bf213639cf16e99c046c7704b05
rafinhaa <rafinhaa@me.com>
2022-08-17 11:02:09 -0300

    feat: Implementa componente Header

--- src/Layout/DefaultLayout/index.tsx
+++ src/Layout/DefaultLayout/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header";
 import { Container } from "./styles";
 
 export const DefaultLayout = () => (
   <Container>
+    <Header />
     <Outlet />
   </Container>
 );
