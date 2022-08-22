db1a0ac19a0d88121b6014360c3376bd26c1337b
rafinhaa <rafinhaa@me.com>
2022-08-22 10:53:01 -0300

    feat: Cria seção OurCoffees

---
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "../../../../components/Typography";
+import { OurCoffeesContent } from "./styles";
+
+export const OurCoffees = () => {
+  return (
+    <OurCoffeesContent>
+      <Typography.Title size="l" color="subtitle">
+        Nossos cafés
+      </Typography.Title>
+    </OurCoffeesContent>
+  );
+};
