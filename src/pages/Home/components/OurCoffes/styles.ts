335ca511abc95a2655e0c741b859f767be268008
Rafael Rodrigues <rafinhaa@me.com>
2022-08-22 10:56:36 -0300

    Revert "feat: Cria seção OurCoffees"
    This reverts commit 3b941022028ff911493c4beb1d4819d9b0d1f742.

--- src/pages/Home/components/OurCoffes/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const OurCoffeesContent = styled.section`
-  width: 100%;
-  margin-top: 2rem;
-`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,9 @@
 import { Intro } from "./components/Intro";
-import { OurCoffes } from "./components/OurCoffes";
 
 export const Home = () => {
   return (
     <div>
       <Intro />
-      <OurCoffees />
     </div>
   );
 };
