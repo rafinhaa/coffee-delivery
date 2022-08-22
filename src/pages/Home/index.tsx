91565e118dcc4a3da016caf2481588aea83fff83
rafinhaa <rafinhaa@me.com>
2022-08-22 10:53:01 -0300

    feat: Cria seção OurCoffees

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Intro } from "./components/Intro";
+import { OurCoffes } from "./components/OurCoffes";
 
 export const Home = () => {
   return (
     <div>
       <Intro />
+      <OurCoffees />
     </div>
   );
 };
