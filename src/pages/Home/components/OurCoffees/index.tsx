d36c38b18208d9f319de5803367e7b6f28fe6666
rafinhaa <rafinhaa@me.com>
2022-08-23 12:05:06 -0300

    feat: Implementa componente CoffeeCard

--- src/pages/Home/components/OurCoffees/index.tsx
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -1,5 +1,7 @@
 import { Typography } from "../../../../components/Typography";
-import { OurCoffeesContent } from "./styles";
+import { coffees } from "../../../../data/coffees";
+import { CoffeeCard } from "../CoffeeCard";
+import { CoffeeList, OurCoffeesContent } from "./styles";
 
 export const OurCoffees = () => {
   return (
@@ -7,6 +9,11 @@ export const OurCoffees = () => {
       <Typography.Title size="l" color="subtitle">
         Nossos cafés
       </Typography.Title>
+      <CoffeeList>
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee.id} coffee={coffee} />
+        ))}
+      </CoffeeList>
     </OurCoffeesContent>
   );
 };
