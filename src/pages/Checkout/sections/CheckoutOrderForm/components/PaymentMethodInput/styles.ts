188d5f5f7279507a129e0ce28e2b125152047a7c
rafinhaa <rafinhaa@me.com>
2022-08-25 18:57:30 -0300

    feat: Cria componente CheckoutOrderForm

---
+++ src/pages/Checkout/sections/CheckoutOrderForm/components/PaymentMethodInput/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const PaymentMethodContainer = styled.div`
+  padding: 0 1rem;
+  background: ${({ theme: { colors } }) => colors["base-button"]};
+  color: ${({ theme: { colors } }) => colors["base-text"]};
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.75rem;
+  font-size: 0.75rem;
+  text-transform: uppercase;
+  border-radius: 6px;
+  height: 3rem;
+  border: 1px solid ${({ theme: { colors } }) => colors["base-button"]};
+  transition: 0.4s;
+
+  &:hover {
+    background: ${({ theme: { colors } }) => colors["base-hover"]};
+  }
+  svg {
+    color: ${({ theme: { colors } }) => colors["purple"]};
+  }
+
+  user-select: none;
+`;
---
+++ src/pages/Checkout/sections/CheckoutOrderForm/components/PaymentMethodInput/types.ts
@@ -0,0 +1,3 @@
+export type TPaymentMethodInput = {
+  type: "card" | "bank" | "cash";
+};
