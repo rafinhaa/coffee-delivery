5ce11002ec1738a48d227d4bdea2a8534cc45cee
rafinhaa <rafinhaa@me.com>
2022-08-27 10:37:31 -0300

    feat: Implementa section SelectedCoffees

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,12 @@
 import { CheckoutOrderForm } from "./sections/CheckoutOrderForm";
+import { SelectedCoffees } from "./sections/SelectedCoffees";
 import { CheckoutOrderContainer } from "./styles";
 
 export const Checkout = () => {
   return (
     <CheckoutOrderContainer>
       <CheckoutOrderForm />
+      <SelectedCoffees />
     </CheckoutOrderContainer>
   );
 };
