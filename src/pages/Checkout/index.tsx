a4eb5714d567b8a3ce164354d2d7b4b27f89b36e
rafinhaa <rafinhaa@me.com>
2022-08-28 17:48:39 -0300

    feat: Implementa verificação de formulário

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,56 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { CheckoutOrderForm } from "./sections/CheckoutOrderForm";
 import { SelectedCoffees } from "./sections/SelectedCoffees";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FormProvider } from "react-hook-form";
+
 import { CheckoutOrderContainer } from "./styles";
 
+enum PaymentMethods {
+  card = "card",
+  bank = "bank",
+  cash = "cash",
+}
+
+const checkoutOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, "Informe o cep"),
+  street: zod.string().min(1, "Informe a rua"),
+  number: zod.string().min(1, "Informe o número"),
+  complement: zod.string(),
+  locale: zod.string().min(1, "Informe o bairro"),
+  city: zod.string().min(1, "Informe a cidade"),
+  uf: zod.string().min(1, "Informe o uf"),
+  paymentMethod: zod.nativeEnum(PaymentMethods, {
+    errorMap: () => {
+      return {
+        message: "Informe o método de pagamento",
+      };
+    },
+  }),
+});
+
+export type OderData = zod.infer<typeof checkoutOrderFormValidationSchema>;
+
+type CheckoutOrderFormData = OderData;
+
 export const Checkout = () => {
+  const checkoutOrderForm = useForm<CheckoutOrderFormData>({
+    resolver: zodResolver(checkoutOrderFormValidationSchema),
+  });
+
+  const { handleSubmit } = checkoutOrderForm;
+
+  const handleCheckoutOrder = (data: CheckoutOrderFormData) => {
+    console.log(data);
+  };
+
   return (
-    <CheckoutOrderContainer>
-      <CheckoutOrderForm />
-      <SelectedCoffees />
-    </CheckoutOrderContainer>
+    <FormProvider {...checkoutOrderForm}>
+      <CheckoutOrderContainer onSubmit={handleSubmit(handleCheckoutOrder)}>
+        <CheckoutOrderForm />
+        <SelectedCoffees />
+      </CheckoutOrderContainer>
+    </FormProvider>
   );
 };
