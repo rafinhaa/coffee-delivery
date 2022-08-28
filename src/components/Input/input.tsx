ecb0b561c2bb58d242b0c4fa2daa527afaf758e9
rafinhaa <rafinhaa@me.com>
2022-08-28 13:01:02 -0300

    refactor: Componente input

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,6 +1,24 @@
-import { InputStyleContainer } from "./styles";
+import { forwardRef } from "react";
+import { Typography } from "../Typography";
+import {
+  InputStyleContainer,
+  InputStyled,
+  InputWrapper,
+  RightText,
+} from "./styles";
 import { TInputProps } from "./types";
 
-export const Input = ({ ...props }: TInputProps) => {
-  return <InputStyleContainer {...props} />;
-};
+export const Input = forwardRef<HTMLInputElement, TInputProps>(function Input(
+  { error, className, rightText, ...props },
+  ref
+) {
+  return (
+    <InputWrapper className={className}>
+      <InputStyleContainer>
+        <InputStyled {...props} ref={ref} />
+        {rightText && <RightText>{rightText}</RightText>}
+      </InputStyleContainer>
+      {error && <Typography.Text size="s">{error}</Typography.Text>}
+    </InputWrapper>
+  );
+});
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,20 +1,48 @@
 import styled from "styled-components";
 
-export const InputStyleContainer = styled.input`
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  position: relative;
+
+  > p {
+    color ${({ theme: { colors } }) => colors["purple-dark"]}
+  }
+`;
+
+export const InputStyleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   height: 2.625rem;
   border-radius: 4px;
   border: 1px solid ${({ theme: { colors } }) => colors["base-button"]};
   background: ${({ theme: { colors } }) => colors["base-input"]};
+  overflow: hidden;
 
-  &:focus {
+  &:focus-within {
     border-color: ${({ theme: { colors } }) => colors["yellow-dark"]};
   }
+`;
 
-  color: ${({ theme: { colors } }) => colors["base-text"]};
+export const InputStyled = styled.input`
+  flex: 1;
+  height: 100%;
+  background: none;
+  border: none;
   font-size: 0.75rem;
   padding: 0 0.75rem;
+  color: ${({ theme: { colors } }) => colors["base-text"]};
 
   &::placeholder {
     color: ${({ theme: { colors } }) => colors["base-label"]};
   }
+`;
+
+export const RightText = styled.p`
+  font-size: 0.75rem;
+  margin-right: 0.75rem;
+  font-style: italic;
+  color: ${({ theme: { colors } }) => colors["base-label"]};
 `;
