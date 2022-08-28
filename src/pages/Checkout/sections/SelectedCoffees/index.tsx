25bccb196d25e01d6208b51572115d80eb68851a
rafinhaa <rafinhaa@me.com>
2022-08-28 10:21:13 -0300

    feat: Implementa useCart em SelectedCoffees

--- src/pages/Checkout/sections/SelectedCoffees/index.tsx
+++ src/pages/Checkout/sections/SelectedCoffees/index.tsx
@@ -1,19 +1,21 @@
 import { Typography } from "../../../../components/Typography";
+import { useCart } from "../../../../hooks/useCart";
 
 import { Confirmation } from "../Confirmation";
 import { CoffeeCartCard } from "./components/CoffeeCartCard";
 import { DetailsContainer, SelectedCoffeesContainer } from "./styles";
 
 export const SelectedCoffees = () => {
+  const { cartItems } = useCart();
   return (
     <SelectedCoffeesContainer>
       <Typography.Title size="xs" color="subtitle">
         Cafés selecionados
       </Typography.Title>
       <DetailsContainer>
-        <CoffeeCartCard />
-        <CoffeeCartCard />
-        <CoffeeCartCard />
+        {cartItems.map((cartItem) => (
+          <CoffeeCartCard key={cartItem.id} coffee={cartItem} />
+        ))}
         <Confirmation />
       </DetailsContainer>
     </SelectedCoffeesContainer>
