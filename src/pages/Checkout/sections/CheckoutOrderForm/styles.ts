92e4a23b67c670bf5d87da834bda15f563422063
rafinhaa <rafinhaa@me.com>
2022-08-28 17:48:09 -0300

    refactor: Componente CheckoutOrderForm

--- src/pages/Checkout/sections/CheckoutOrderForm/styles.ts
+++ src/pages/Checkout/sections/CheckoutOrderForm/styles.ts
@@ -37,4 +37,8 @@ export const PaymentMethodOptions = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 0.75rem;
+  > p {
+    grid-column: span 3;
+    color: ${({ theme: { colors } }) => colors["purple"]};
+  }
 `;
