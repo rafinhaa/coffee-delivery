7f08c37c1d6b7bfb1849af8816570c0252b06ee8
rafinhaa <rafinhaa@me.com>
2022-08-28 10:56:20 -0300

    feat: Implementa preço total do carrinho

--- src/pages/Checkout/sections/Confirmation/index.tsx
+++ src/pages/Checkout/sections/Confirmation/index.tsx
@@ -1,24 +1,32 @@
 import { Typography } from "../../../../components/Typography";
+import { useCart } from "../../../../hooks/useCart";
+import { formatMoney } from "../../../../utils/formatMoney";
 import { ButtonContainer, ConfirmationSectionContainer } from "./styles";
 
 export const Confirmation = () => {
+  const { cartTotalPriceItens, cartQuantity } = useCart();
+  const deliveryPrice = 13.9;
   return (
     <ConfirmationSectionContainer>
       <div>
         <Typography.Text size="s">Total de itens</Typography.Text>
-        <Typography.Text>R$ 9,90</Typography.Text>
+        <Typography.Text>R$ {formatMoney(cartTotalPriceItens)}</Typography.Text>
       </div>
       <div>
         <Typography.Text size="s">Entrega</Typography.Text>
-        <Typography.Text>R$ 13,50</Typography.Text>
+        <Typography.Text>R$ {formatMoney(deliveryPrice)} </Typography.Text>
       </div>
       <div>
         <Typography.Text size="l" color="subtitle" weight={700}>
           Total
         </Typography.Text>
-        <Typography.Text>R$ 29,90</Typography.Text>
+        <Typography.Text>
+          R$ {formatMoney(cartTotalPriceItens + deliveryPrice)}{" "}
+        </Typography.Text>
       </div>
-      <ButtonContainer>Confirmar Pedido</ButtonContainer>
+      <ButtonContainer disabled={cartQuantity <= 0}>
+        Confirmar Pedido
+      </ButtonContainer>
     </ConfirmationSectionContainer>
   );
 };
